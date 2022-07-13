609ff955f5605f95e7ef58d9e85225995a75b28b
Jai Sawhney <jai.sawhney1@gmail.com>
2022-07-12 19:08:32 -0700

    Sync user draw

--- app.js
+++ app.js
@@ -8,10 +8,18 @@ const io = new Server(server);
 app.set('views', 'static');
 app.set('view engine', 'ejs');
 
-app.use('/static', express.static('static'))
+app.use('/static', express.static('static'));
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index');
 });
 
-server.listen(process.env.PORT || 3000)
+io.on('connection', (socket) => {
+    socket.on('USER_DRAW', (obj) => {
+        socket.broadcast.emit('USER_DRAW', obj);
+    });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Running!")
+});
--- static/client.js
+++ static/client.js
@@ -1,1 +1,47 @@
-alert('Hello World!')
+const socket = io();
+
+const canvas = document.getElementById('canvas');
+const colorSelector = document.getElementById('colorSelector');
+const ctx = canvas.getContext('2d');
+
+let posX = posY = 0
+
+canvas.addEventListener('mousedown', updateCoordinates);
+canvas.addEventListener('mousemove', draw);
+
+function updateCoordinates(e) {
+    posX = e.clientX
+    posY = e.clientY
+}
+
+function draw(e) {
+    if (!e.buttons) return;
+
+    ctx.beginPath();
+    ctx.moveTo(posX, posY);
+
+    const lastPosX = posX
+    const lastPosY = posY;
+
+    updateCoordinates(e);
+    ctx.lineTo(posX, posY);
+    ctx.strokeStyle = colorSelector.value;
+    ctx.stroke();
+
+    socket.emit('USER_DRAW', {
+        posX: posX,
+        posY: posY,
+        lastPosX: lastPosX,
+        lastPosY: lastPosY,
+        color: colorSelector.value
+    });
+}
+
+socket.on('USER_DRAW', function (obj) {
+    ctx.beginPath();
+    ctx.moveTo(obj.lastPosX, obj.lastPosY);
+    ctx.lineTo(obj.posX, obj.posY);
+
+    ctx.strokeStyle = obj.color;
+    ctx.stroke()
+});
